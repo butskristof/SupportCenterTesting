b272afe8946827ec1c37a107d1418858891e5419
Kristof Buts <github@kristofbuts.be>
2019-03-12 08:40:49 +0100

    Finish MVC tests

--- UI-MVC.Tests/TicketControllerTests.cs
+++ UI-MVC.Tests/TicketControllerTests.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using SC.BL;
 using SC.BL.Domain;
+using SC.DAL.Tests;
 using SC.UI.Web.MVC.Controllers;
 using Xunit;
 
@@ -13,13 +14,13 @@ namespace SC.UI.Web.MVC.Tests
 	public class TicketControllerTests
 	{
 		[Fact]
-		public void Index_ReturnsAViewResult_WithAListOfTickets()
+		public void Index_WithTicketsAvailable_ReturnsViewResultWithListOfTickets()
 		{
 			// arrange
 			var mgr = new Mock<ITicketManager>();
 			mgr
 				.Setup(x => x.GetTickets())
-				.Returns(GetTestTickets);
+				.Returns(TestTicketsProvider.GetTestTickets);
 			var controller = new TicketController(mgr.Object);
 
 			// act
@@ -32,16 +33,16 @@ namespace SC.UI.Web.MVC.Tests
 		}
 
 		[Fact]
-		public void Details_ReturnsAViewResult_WithTicket()
+		public void Details_WithValidId_ReturnsViewResultWithTicket()
 		{
 			// arrange
 			int ticketId = 1;
 			var mgr = new Mock<ITicketManager>();
 			mgr
 				.Setup(x => x.GetTicket(ticketId))
-				.Returns(GetTestTickets().FirstOrDefault(ticket => ticket.TicketNumber == ticketId));
+				.Returns(TestTicketsProvider.GetTestTickets().FirstOrDefault(ticket => ticket.TicketNumber == ticketId));
 			var controller = new TicketController(mgr.Object);
-			var ticketThatShouldBeReturned = GetTestTickets().First(t => t.TicketNumber == ticketId);
+			var ticketThatShouldBeReturned = TestTicketsProvider.GetTestTickets().First(t => t.TicketNumber == ticketId);
 			
 			// act
 			var result = controller.Details(ticketId);
@@ -60,7 +61,7 @@ namespace SC.UI.Web.MVC.Tests
 		}
 
 		[Fact]
-		public void Details_ThrowsNullReferenceException_WhenTicketNotFound()
+		public void Details_WithInvalidId_ThrowsNullReferenceException()
 		{
 			// arrange
 			int ticketId = 1;
@@ -78,16 +79,16 @@ namespace SC.UI.Web.MVC.Tests
 		}
 		
 		[Fact]
-		public void Edit_ReturnsAViewResult_WithTicket()
+		public void Edit_WithValidId_ReturnsViewResultWithTicket()
 		{
 			// arrange
 			int ticketId = 1;
 			var mgr = new Mock<ITicketManager>();
 			mgr
 				.Setup(x => x.GetTicket(ticketId))
-				.Returns(GetTestTickets().FirstOrDefault(ticket => ticket.TicketNumber == ticketId));
+				.Returns(TestTicketsProvider.GetTestTickets().FirstOrDefault(ticket => ticket.TicketNumber == ticketId));
 			var controller = new TicketController(mgr.Object);
-			var ticketThatShouldBeReturned = GetTestTickets().First(t => t.TicketNumber == ticketId);
+			var ticketThatShouldBeReturned = TestTicketsProvider.GetTestTickets().First(t => t.TicketNumber == ticketId);
 			
 			// act
 			var result = controller.Edit(ticketId);
@@ -106,7 +107,7 @@ namespace SC.UI.Web.MVC.Tests
 		}
 		
 		[Fact]
-		public void Edit_ReturnsNullAsViewResult_WhenTicketNotFound()
+		public void Edit_WithInvalidId_ReturnsNullAsViewResult()
 		{
 			// arrange
 			int ticketId = 1;
@@ -124,85 +125,5 @@ namespace SC.UI.Web.MVC.Tests
 			Assert.Null(viewResult.Model);
 		}
 		
-		private List<Ticket> GetTestTickets()
-		{
-			var tickets = new List<Ticket>();
-			
-            // Create first ticket with three responses
-            Ticket t1 = new Ticket()
-            {
-	            TicketNumber = 1,
-                AccountId = 1,
-                Text = "Ik kan mij niet aanmelden op de webmail",
-                DateOpened = new DateTime(2017, 9, 9, 13, 5, 59),
-                State = TicketState.Open,
-                Responses = new List<TicketResponse>()
-            };
-            tickets.Add(t1);
-
-            TicketResponse t1r1 = new TicketResponse()
-            {
-                Ticket = t1,
-                Text = "Account was geblokkeerd",
-                Date = new DateTime(2017, 9, 9, 13, 24, 48),
-                IsClientResponse = false
-            };
-            t1.Responses.Add(t1r1);
-
-            TicketResponse t1r2 = new TicketResponse()
-            {
-                Ticket = t1,
-                Text = "Account terug in orde en nieuw paswoord ingesteld",
-                Date = new DateTime(2017, 9, 9, 13, 29, 11),
-                IsClientResponse = false
-            };
-            t1.Responses.Add(t1r2);
-
-            TicketResponse t1r3 = new TicketResponse()
-            {
-                Ticket = t1,
-                Text = "Aanmelden gelukt en paswoord gewijzigd",
-                Date = new DateTime(2017, 9, 10, 7, 22, 36),
-                IsClientResponse = true
-            };
-            t1.Responses.Add(t1r3);
-            t1.State = TicketState.Closed;
-
-            // Create second ticket with one response
-            Ticket t2 = new Ticket()
-            {
-	            TicketNumber = 2,
-                AccountId = 1,
-                Text = "Geen internetverbinding",
-                DateOpened = new DateTime(2017, 11, 5, 9, 45, 13),
-                State = TicketState.Open,
-                Responses = new List<TicketResponse>()
-            };
-            tickets.Add(t2);
-
-            TicketResponse t2r1 = new TicketResponse()
-            {
-                Ticket = t2,
-                Text = "Controleer of de kabel goed is aangesloten",
-                Date = new DateTime(2017, 11, 5, 11, 25, 42),
-                IsClientResponse = false
-            };
-            t2.Responses.Add(t2r1);
-            t2.State = TicketState.Answered;
-
-            // Create hardware ticket without response
-            HardwareTicket ht1 = new HardwareTicket()
-            {
-	            TicketNumber = 3,
-                AccountId = 2,
-                Text = "Blue screen!",
-                DateOpened = new DateTime(2017, 12, 14, 19, 5, 2),
-                State = TicketState.Open,
-                DeviceName = "PC-123456"
-            };
-            tickets.Add(ht1);
-
-			return tickets;
-		}
 	}
 }
